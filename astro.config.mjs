1f4a772a8cd530afe66a16262788228a36b14d8d
Kananek Thongkam <info.dvgamer@gmail.com>
2025-05-08 00:16:09 +0700

    feat: add Tailwind CSS and Meyda for speaker identification demo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@tailwindcss/vite'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    assets: 'dist',
+  },
+  vite: {
+    plugins: [tailwind()],
+  }
+});
